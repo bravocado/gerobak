a7b1634529e61df418ed93e9cc26734a97ef2ea2
Bravocado <bravocado.project@gmail.com>
2015-04-20 13:26:36 +0700

    get the data!

--- test/tests.js
+++ test/tests.js
@@ -3,4 +3,31 @@
 test('Values', function(){
 	'use strict';
 	ok(true, 'passes');
+
+	console.log(gerobak.getOpt());
+
+
+	var data = gerobak.getOpt();
+
+	function toObject(data) {
+		var keys = data;
+		var i = 0;
+		var k = 0;
+		var obj =  null;
+		var output = [];
+
+
+		for (k = 0; k < keys.length; k++) {
+			obj = {};
+			obj[keys[k][i]] = data[k][1];
+
+			output.push(obj);
+		}
+
+		return output;
+
+	}
+
+	console.log(toObject(data));
+
 });
