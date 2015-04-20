67a19a927a825cc248df7789c146317fe6858782
Bravocado <bravocado.project@gmail.com>
2015-04-21 00:47:13 +0700

    now gerobak can parse the basket option.

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,6 @@
-/*global console, gerobak, test, ok*/
+/*global test, ok*/
 
 test('Values', function(){
 	'use strict';
 	ok(true, 'passes');
-	console.log(gerobak.getUrl());
-	console.log(gerobak.getOpt());
 });
